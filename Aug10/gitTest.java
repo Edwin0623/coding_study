a89fcab41ac8ead3654104f1013c855e65c2905d
Muhyun <moody920623@gmail.com>
2019-08-10 23:04:24 -0700

    change text

--- Aug10/gitTest.java
+++ Aug10/gitTest.java
@@ -2,6 +2,19 @@ package Aug10;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("A mu gu na");
+        int[][] data = { {1,2,3} , {4,5,6} };
+
+        for (int i = 0; i < data[0].length; i++) {
+            int store = data[0][i];
+            data[0][i] = data[1][i];
+            data[1][i] = store;
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0 ; j < data[i].length; j++) {
+                System.out.print(data[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
