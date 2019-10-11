5b4b3b753f3b7c8cca76bc6379abf91867fc70fe
Muhyun Kim <moody920623@gmail.com>
2019-10-11 13:07:57 +0900

    virus game

--- Sep8/virus.java
+++ Sep8/virus.java
@@ -8,51 +8,22 @@ public class virus {
 
         // make a map
         int[][] map = getMap();
+        // turn is started from 1.
         int turn = 1;
+        int player1Score = 0;
+        int player2Score = 0;
 
-        while (true) {
-            // turn is started from 1.
-            System.out.println("Player " + turn + "'s turn!");
-            showScore(map);
-            printMap(map);
-
-            if (checkScoreIsZero(showScore(map))) {
-                System.out.println("Game Over" + "\n" + "Player " + turn + " win!");
-                break;
-            } else {
-                // Get source coordinates of the cell of Player want to move.
-                System.out.print("Input 'x' to get a cell on the map: ");
-                int x_virus = sc.nextInt();
-                System.out.print("Input 'y' to get a cell on the map: ");
-                int y_virus = sc.nextInt();
-
-                // 맵 밖 좌표를 찍으면 다시 찍으라고 해줌
-                if (x_virus >= 0 && y_virus >= 0 && x_virus < map.length && y_virus < map.length) {
-                    // 플레이어 1인데 2 장기말을 선택하면 다시 선택하라고 해줌
-                    if (map[x_virus][y_virus] == turn) {
-                        game(map, x_virus, y_virus);
-                        turn = (turn % 2) + 1;
-                    } else {
-                        System.out.println("Error: You must select player " + turn + "'s 장기말" + "\n");
-                        continue;
-                    }
-                } else {
-                    System.out.println("Error: Put x,y inside of map!!" + "\n");
-                    continue;
-                }
-            }
-
-        }
+        game(map, turn, player1Score, player2Score);
     }
     public static int[][] getMap() {
-        int[][] map = {{1, 2, 0, 0, 0, 0, 1, 1},
-                       {0, 0, 0, 0, 0, 0, 1, 0},
-                       {0, 0, 0, 0, 0, 0, 1, 1},
+        int[][] map = {{1, 0, 0, 0, 0, 0, 1, 2},
+                       {0, 0, 0, 0, 0, 1, 1, 1},
+                       {0, 0, 0, 0, 0, 1, 1, 1},
                        {0, 0, 0, 0, 0, 0, 0, 0},
                        {0, 0, 0, 0, 0, 0, 0, 0},
-                       {0, 0, 0, 0, 0, 0, 0, 0},
-                       {0, 0, 0, 0, 0, 0, 0, 0},
-                       {1, 0, 0, 0, 0, 0, 0, 1}};
+                       {1, 1, 1, 0, 0, 0, 0, 0},
+                       {1, 1, 1, 0, 0, 0, 0, 0},
+                       {2, 1, 1, 0, 0, 0, 0, 1}};
         return map;
     }
     public static void printMap(int[][] arr) {
@@ -64,104 +35,205 @@ public class virus {
         }
         System.out.println();
     }
-    public static int getCellDist(int x_virus, int y_virus, int x_new, int y_new) {
+    public static int getCellDist(int x_cell, int y_cell, int x_new, int y_new) {
         int dist;
-        if (Math.abs(x_new - x_virus) <= Math.abs(y_new - y_virus)) {
-            dist = y_new - y_virus;
+        if (Math.abs(x_new - x_cell) <= Math.abs(y_new - y_cell)) {
+            dist = Math.abs(y_new - y_cell);
         } else {
-            dist = x_new - x_virus;
-        }
-
-        if (dist < 0) {
-            dist = dist * -1;
+            dist = Math.abs(x_new - x_cell);
         }
         return dist;
     }
-    public static int[] showScore(int[][] arr) {
-        String player1Score = "";
-        String player2Score = "";
+    public static int[] showScore(int[][] arr, int player1Score, int player2Score) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 if (arr[i][j] == 1) {
-                    player1Score = player1Score.concat("1");
+                    player1Score++;
                 }
                 if (arr[i][j] == 2) {
-                    player2Score = player2Score.concat("2");
+                    player2Score++;
                 }
             }
         }
-        System.out.println(player1Score.length() + " vs " + player2Score.length());
-        int[] score = {player1Score.length(), player2Score.length()};
-        return score;
+        System.out.println(player1Score + " vs " + player2Score);
+        int[] scoreArr = {player1Score, player2Score};
+        return scoreArr;
     }
-    public static boolean checkScoreIsZero(int[] score) {
+    public static boolean checkScoreIsZero(int[] scoreArr) {
         boolean scoreCheck = false;
-        if (score[0] == 0 || score[1] == 0) {
+        if (scoreArr[0] == 0 || scoreArr[1] == 0) {
             scoreCheck = true;
         }
         return scoreCheck;
     }
-    public static void greyOn(int[][] arr, int x_virus, int y_virus) {
+    public static boolean checkSurroundTheCell(int[][] arr, int x, int y) {
+        // Check surround that can be shown 5 to move user's cell
+        boolean checkSurround = false;
         for (int i = -2; i <= 2; i++) {
             for (int j = -2; j <= 2; j++) {
-                if (x_virus - i >= 0 && y_virus - j >= 0 && x_virus - i < arr.length && y_virus - j < arr[0].length) {
-                    if (arr[x_virus - i][y_virus - j] == 0) {
-                        arr[x_virus - i][y_virus - j] = 5;
+                if (x - i >= 0 && y - j >= 0 && x - i < arr.length && y - j < arr[0].length) {
+                    if (arr[x - i][y - j] == 0) {
+                        checkSurround = true;
+                    }
+                }
+            }
+        }
+        return checkSurround;
+    }
+    public static boolean checkWholeMapFor1(int[][] arr) {
+        boolean checkWholeMapFor1 = false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (arr[i][j] == 1 && checkSurroundTheCell(arr, i, j) == true) {
+                    checkWholeMapFor1 = true;
+                }
+            }
+        }
+        return checkWholeMapFor1;
+    }
+    public static boolean checkWholeMapFor2(int[][] arr) {
+        boolean checkWholeMapFor2 = false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (arr[i][j] == 2 && checkSurroundTheCell(arr, i, j) == true) {
+                    checkWholeMapFor2 = true;
+                }
+            }
+        }
+        return checkWholeMapFor2;
+    }
+    public static void greyOn(int[][] arr, int x, int y) {
+        // show 5 user can move
+        for (int i = -2; i <= 2; i++) {
+            for (int j = -2; j <= 2; j++) {
+                if (x - i >= 0 && y - j >= 0 && x - i < arr.length && y - j < arr[0].length) {
+                    if (arr[x - i][y - j] == 0) {
+                        arr[x - i][y - j] = 5;
                     }
                 }
             }
         }
         printMap(arr);
     }
-    public static void greyOff(int[][] arr, int x_virus, int y_virus) {
+    public static void greyOff(int[][] arr, int x, int y) {
+        // Off 5
         for (int i = -2; i <= 2; i++) {
             for (int j = -2; j <= 2; j++) {
-                if (x_virus - i >= 0 && y_virus - j >= 0 && x_virus - i < arr.length && y_virus - j < arr[0].length) {
-                    if (arr[x_virus - i][y_virus - j] != 1 && arr[x_virus - i][y_virus - j] != 2) {
-                        arr[x_virus - i][y_virus - j] = 0;
+                if (x - i >= 0 && y - j >= 0 && x - i < arr.length && y - j < arr[0].length) {
+                    if (arr[x - i][y - j] != 1 && arr[x - i][y - j] != 2) {
+                        arr[x - i][y - j] = 0;
                     }
                 }
             }
         }
     }
-    public static void virus(int[][] arr, int x_new, int y_new) {
+
+    public static void virus(int[][] arr, int x_new, int y_new, int turn) {
         // Infect surrounding cells after moving to the destination coordinates.
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
                 if (x_new - i >= 0 && y_new - j >= 0 && x_new - i < arr.length && y_new - j < arr[0].length) {
                     // 이동 했을 때 주위 흰색은 강몀 안되게 하는 코드
                     if (arr[x_new - i][y_new - j] != 0) {
-                        arr[x_new - i][y_new - j] = arr[x_new][y_new];
+                        arr[x_new - i][y_new - j] = turn;
                     }
                 }
             }
         }
-    }
-    public static void game(int[][] map, int x_virus, int y_virus) {
-        Scanner sc = new Scanner(System.in);
-        greyOn(map, x_virus, y_virus);
-
-        // Get destination coordinates of the cell I want to move to.
-        System.out.print("Input 'x' you want to move: ");
-        int x_new = sc.nextInt();
-        System.out.print("Input 'y' you want to move: ");
-        int y_new = sc.nextInt();
-        int dist = getCellDist(x_virus, y_virus, x_new, y_new);
-
-        if (map[x_new][y_new] == 5) {
-            if (dist == 1) {
-                map[x_new][y_new] = map[x_virus][y_virus];
-            } else if (dist == 2) {
-                map[x_new][y_new] = map[x_virus][y_virus];
-                map[x_virus][y_virus] = 0;
-            } else {
-                System.out.println("You tried to move too much");
-            }
-            greyOff(map, x_virus, y_virus);
-            virus(map, x_new, y_new);
-        } else {
-            System.out.println("You can not put there");
-        }
         System.out.println();
+    }
+    public static void game(int[][] map, int turn, int player1Score, int player2Score) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            if (checkScoreIsZero(showScore(map, player1Score, player2Score)) || (checkWholeMapFor1(map) && checkWholeMapFor2(map))) {
+                printMap(map);
+                System.out.println("Player " + turn + "'s turn!");
+                // Get source coordinates of the cell of Player want to move.
+                System. out.print("Input 'x' to get a cell on the map: ");
+                int x_cell = sc.nextInt();
+                System.out.print("Input 'y' to get a cell on the map: ");
+                int y_cell = sc.nextInt();
+
+                // 맵 밖 좌표를 찍으면 다시 찍으라고 해줌
+                if (x_cell >= 0 && y_cell >= 0 && x_cell < map.length && y_cell < map.length) {
+                    // 자신의 'turn'에 맞는 'cell'을 골랐는지 먼저 체크 한다
+                    if (map[x_cell][y_cell] == turn) {
+
+                        // 자신의 'cell'을 골랐고, 주위에 0이 1개라도 있으면 옮길 수 있는 곳을 보여 준다
+                        if (checkSurroundTheCell(map, x_cell, y_cell)) {
+                            greyOn(map, x_cell, y_cell);
+                        } else {
+                            System.out.println("Error: You can not choose it!!" + "\n");
+                            continue;
+                        }
+
+                        while (true) {
+                            // Get destination coordinates of the cell I want to move to.
+                            System.out.print("Input 'x' you want to move: ");
+                            int x_new = sc.nextInt();
+                            System.out.print("Input 'y' you want to move: ");
+                            int y_new = sc.nextInt();
+                            int dist = getCellDist(x_cell, y_cell, x_new, y_new);
+
+                            // 5를 고를 떄 -> 'turn'이 끝나는 유일한 조건
+                            if (map[x_new][y_new] == 5) {
+                                // 1칸이면 복사하면서 이동
+                                if (dist == 1) {
+                                    map[x_new][y_new] = turn;
+                                }
+                                // 2칸이면 이동
+                                if (dist == 2) {
+                                    map[x_new][y_new] = turn;
+                                    map[x_cell][y_cell] = 0;
+                                }
+                                greyOff(map, x_cell, y_cell);
+                                // 이동한 뒤에 주위 감염 시킴
+                                virus(map, x_new, y_new, turn);
+                                // 턴 종료!
+                                break;
+                            }
+                            // 자신의 'Cell'을 또 고를 때
+                            else if (map[x_new][y_new] == turn) {
+                                greyOff(map, x_cell, y_cell);
+                                x_cell = x_new;
+                                y_cell = y_new;
+                                greyOn(map, x_new, y_new);
+                                // 이동할 곳 다시 고르게 하기위해 while loop 처음으로 이동
+                                continue;
+                            }
+                            // 거리가 2칸보다 클 때
+                            else if (dist > 2 && map[x_new][y_new] == 0) {
+                                System.out.println("Error: You tried to move too much!!" + "\n");
+                                printMap(map);
+                                // 이동할 곳 다시 고르게 하기위해 while loop 처음으로 이동
+                                continue;
+                            }
+                            // 상대방 'cell'에 놓으려고 할 때
+                            else {
+                                System.out.println("Error: You can not put there!" + "\n");
+                                printMap(map);
+                                // 이동할 곳 다시 고르게 하기위해 while loop 처음으로 이동
+                                continue;
+                            }
+                        }
+                    } else {
+                        // 각자의 'turn'에 맞는 'cell'을 고르지 않을 때
+                        System.out.println("Error: Choose your cell!!" + "\n");
+                        continue;
+                    }
+                } else {
+                    // 맵 밖에 클릭 할 때
+                    System.out.println("Error: Choose a cell inside of map!!" + "\n");
+                    continue;
+                }
+                turn = turn % 2 + 1;
+            } else {
+                printMap(map);
+                showScore(map, player1Score, player2Score);
+                System.out.println("Game Ended!" + "\n" + "player " + (turn % 2 + 1) + " is win!!");
+                break;
+            }
+        }
     }
 }
